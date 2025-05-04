8492f942c6518cdc25a9a942af81d91489dc974d
Iury <Iuryhattori.prog@gmail.com>
2025-05-04 14:32:57 -0300

    Update Funcoes.hs

--- Funcoes.hs
+++ Funcoes.hs
@@ -7,13 +7,16 @@ adicionarlivro liv x =  if elem liv x
                         then Left "Erro! Livro já registrado"
                         else Right (liv : x)
 
-listarlivros :: Livro -> String
-listarlivros mostrarlivros =
-    "Título: " ++ (titulo mostrarlivros) ++ "; " ++ 
-    "Autor: " ++ (autor mostrarlivros) ++ "; " ++
-    "Ano: " ++ show (ano mostrarlivros) ++ "; " ++
-    "Código Único: " ++ show (cod mostrarlivros) ++ "; " ++
-    "Status: " ++ show (status mostrarlivros)
+coutlivro :: Livro -> String -- função que lista apenas 1 livro, a intenção é usa-la como função auxiliar
+coutlivro mostrarlivro =
+    "Título: " ++ (titulo mostrarlivro) ++ "; " ++ 
+    "Autor: " ++ (autor mostrarlivro) ++ "; " ++
+    "Ano: " ++ show (ano mostrarlivro) ++ "; " ++
+    "Código Único: " ++ show (cod mostrarlivro) ++ "; " ++
+    "Status: " ++ show (status mostrarlivro)
+
+listarlivros :: [Livro] -> String
+listarlivros livros = unlines (map coutlivro livros)
 
 removerLivro :: Livro -> [Livro] -> Either String [Livro]
 removerLivro liv x = if elem liv x
@@ -26,11 +29,14 @@ adicionarusuario us x = if elem us x
                         then Left "Erro! Usuário já cadastrado"
                         else Right (us : x)
 
-listarusuarios :: User -> String
-listarusuarios mostrarUser =
+coutusuarios :: User -> String
+coutusuarios mostrarUser =
     "Nome: " ++ show (nome mostrarUser) ++ "; " ++ 
     "Matrícula: " ++ show ( matricula mostrarUser) ++ "; " ++
     "Email: " ++ show (email mostrarUser) 
+
+listarusuarios :: [User] -> String
+listarusuarios users = unlines (map coutusuarios users)
 
 removerusuario :: User -> [User] -> Either String [User]
 removerusuario us x =   if elem us x
@@ -51,4 +57,10 @@ listaespera :: User -> Fila -> Either String Fila
 listaespera user queue =    if elem user (usuarios queue)
                             then Left "Erro! Usuário já está na fila"
                             else Right queue { usuarios = user : usuarios queue}
+
+
+exibirlistaespera :: Fila -> String
+exibirlistaespera fil =
+    unlines (map coutusuarios (usuarios fil)) ++
+    "\nTotal de usuários fila: " ++ show (length (usuarios fil))
 
