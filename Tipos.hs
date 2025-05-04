773d876557c54700942327f042f930bb462a0b9b
Iury <Iuryhattori.prog@gmail.com>
2025-05-04 02:01:32 -0300

    Create Tipos.hs
    Este módulo define os tipos de dados que serão utilizados ao longo do projeto.
    Foram definidos os tipos Status, Livro, User e Fila.
    O tipo Fila foi implementado como um registrador contendo uma lista de usuários (User), isso vai facilitar a manipulação da fila, pois ela está encapsulada em uma estrutura única.

---
+++ Tipos.hs
@@ -0,0 +1,25 @@
+module Tipos where
+
+data Status = Disponivel | Indisponivel | Emprestado deriving(Show, Read, Eq)
+
+data Livro = Livro
+    { titulo    :: String
+    , autor     :: String
+    , ano       :: String
+    , cod       :: Int
+    , status    :: Status
+    } deriving (Show, Read, Eq)
+
+data User = User
+    { nome      :: String
+    , matricula :: Int
+    , email     :: String
+    } deriving (Show, Read, Eq)
+
+data Fila = Fila
+    {
+        usuarios :: [User]
+    }
+
+
+   
