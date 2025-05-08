2c03155deec58dec3ff31686fc5831035eee7091
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-05-08 10:51:38 -0300

    Update Persistencias.hs

--- Persistencias.hs
+++ Persistencias.hs
@@ -2,7 +2,8 @@ module Persistencias(
     salvarEmArquivoLivro,
     carregarDeArquivoLivro,
     carregarDeArquivoUser,
-    salvarEmArquivoUser
+    salvarEmArquivoUser,
+    salvar_criarBiblioteca
 ) where
 import Tipos
 import Funcoes
@@ -10,6 +11,30 @@ import System.IO
 import Data.Maybe (mapMaybe)
 import Text.Read (readMaybe)
 import Control.Exception (evaluate)
+import System.Directory (doesFileExist)
+import Data.Char (toUpper)
+
+salvar_criarBiblioteca :: [Livro] -> IO ()
+salvar_criarBiblioteca livros = do
+    let nome = "biblioteca.txt"
+    existe <- doesFileExist nome
+    if existe then do
+        putStrLn "Arquivo biblioteca.txt já existe, deseja sobrescrevê-lo? (S/N)"
+        resposta <- getLine
+        if map toUpper resposta == "S" then do
+            salvarEmArquivoLivro nome livros
+            putStrLn "Até a próxima"
+        else
+            putStrLn "Até a próxima"
+    else do
+        putStrLn "O arquivo biblioteca.txt não existe, deseja criar um novo? (S/N)"
+        resposta <- getLine
+        if map toUpper resposta == "S" then do
+            salvarEmArquivoLivro nome livros
+            putStrLn "Até a próxima"
+        else
+            putStrLn "Até a próxima"
+
 
 stringParaLivro :: String -> Maybe Livro
 stringParaLivro = readMaybe
@@ -20,13 +45,25 @@ stringParaUser = readMaybe
 stringParaFila :: String -> Maybe Fila
 stringParaFila = readMaybe
 
+--carregarDeArquivoLivro :: FilePath -> IO [Livro]
+--carregarDeArquivoLivro ca = do
+--    co <- readFile ca
+--    let l = lines co
+--        livros = mapMaybe stringParaLivro l
+--    evaluate (length livros)
+--    return livros
+
 carregarDeArquivoLivro :: FilePath -> IO [Livro]
 carregarDeArquivoLivro ca = do
-    co <- readFile ca
-    let l = lines co
-        livros = mapMaybe stringParaLivro l
-    evaluate (length livros)
-    return livros
+    existe <- doesFileExist ca
+    if not existe
+        then return []
+        else do
+            co <- readFile ca
+            let l = lines co
+                livros = mapMaybe stringParaLivro l
+            evaluate (length livros)
+            return livros
 
 salvarEmArquivoLivro :: FilePath -> [Livro] -> IO ()
 salvarEmArquivoLivro ca co = do
